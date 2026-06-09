b49576343a0ec21a48ae2ab543fcece31e16f5a6
James Tippett <james.tippett@gmail.com>
2026-06-09 11:25:49 +0700

    feat: Message

--- src/cr_llm.cr
+++ src/cr_llm.cr
@@ -2,6 +2,8 @@ require "json"
 require "./req_llm/error"
 require "./req_llm/keys"
 require "./req_llm/content_part"
+require "./req_llm/tool_call"
+require "./req_llm/message"
 
 module ReqLLM
   VERSION = "0.1.0"
---
+++ src/req_llm/tool_call.cr
@@ -0,0 +1,7 @@
+module ReqLLM
+  # Forward-declared empty stub so Message/Context compile before Unit D.
+  # Unit D (plan Task 7) reopens this struct with id/name/arguments/metadata
+  # plus to_wire/from_wire helpers.
+  struct ToolCall
+  end
+end
