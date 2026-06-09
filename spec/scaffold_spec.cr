646d4f68ae1b6f956e1ae9381892d81c09daa969
James Tippett <james.tippett@gmail.com>
2026-06-09 11:12:56 +0700

    chore: scaffold cr_llm shard

---
+++ spec/scaffold_spec.cr
@@ -0,0 +1,7 @@
+require "./spec_helper"
+
+describe ReqLLM do
+  it "has a version" do
+    ReqLLM::VERSION.should eq("0.1.0")
+  end
+end
---
+++ src/cr_llm.cr
@@ -0,0 +1,9 @@
+require "json"
+require "./req_llm/error"
+
+module ReqLLM
+  VERSION = "0.1.0"
+end
+
+module LLMDB
+end
